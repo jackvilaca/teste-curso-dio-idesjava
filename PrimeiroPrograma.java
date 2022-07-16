8c32c9ae208e69b30858ecce508acf1e46e3c83d
Jaqueline <jackvilaca@gmail.com>
2022-07-15 23:35:17 -0300

    first commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+
+public class PrimeiroPrograma {
+			
+	public static void main(String[] args) {
+		
+		Gato Gato = new Gato();
+		Livros Livros = new Livros ();
+		
+		System.out.println(Gato);
+		System.out.println(Livros);
+		
+		
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
